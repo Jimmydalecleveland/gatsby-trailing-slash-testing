a93831f3854b333ac285e8b7533b93bdcd95be25
Jimmy DC <jimmydalecleveland@gmail.com>
2020-11-18 10:25:11 -0700

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,7 @@ module.exports = {
     'gatsby-plugin-remove-trailing-slashes',
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-meta-redirect',
+    'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
